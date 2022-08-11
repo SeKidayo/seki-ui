42128859c45cec4fc1b2ff5631c21dac205d7a24
seki <seki4649@163.com>
2022-08-11 14:30:11 +0800

    style:添加CSS变量

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,9 @@
         <div>右侧nav</div>
       </div>
     </header>
+    <aside class="home-aside">
+      <div class="home-aside-content">侧边栏</div>
+    </aside>
     <main class="home-container">
       <section>1</section>
       <section>2</section>
@@ -28,26 +31,41 @@ export default {
     top: 0;
     width: 100%;
     background-color: aqua;
-    height: 55px;
+    height: var(--header-height);
 
     .home-header-content {
       background-color: antiquewhite;
       display: flex;
       justify-content: space-between;
-      max-width: 1400px;
+      max-width: var(--header-content-max-width);
       margin: 0 auto;
+    }
+  }
+
+  .home-aside {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    top: var(--header-height);
+    background-color: rosybrown;
+    width: var(--aside-width);
+    overflow-y: auto;
+
+    .home-aside-content {
+      height: 2000px;
     }
   }
 
   .home-container {
     background-color: aquamarine;
-    padding-top: 55px;
-
+    padding-top: var(--header-height);
+    padding-left: var(--aside-width);
     height: 2000px;
   }
 
   .home-footer {
     background-color: beige;
+    padding-left: var(--aside-width);
   }
 }
 </style>
