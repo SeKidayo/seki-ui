37297e918e739f4398e5c2c4afde1790b31b4e1b
seki <seki4649@163.com>
2022-08-19 21:36:38 +0800

    fix:声明文件配置修正

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -4,5 +4,4 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
-// 解决报错: 无法找到模块“mockjs”的声明文件。尝试使用 `npm i --save-dev @types/mockjs` (如果存在)，或者添加一个包含 `declare module 'mockjs';` 的新声明(.d.ts)文件ts(7016)
-declare module 'mockjs';
+
