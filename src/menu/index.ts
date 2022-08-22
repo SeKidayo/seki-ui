906ab8b794b5958ccca047ae8466a8f16b724194
seki <seki4649@163.com>
2022-08-22 20:51:35 +0800

    refactor:调整menu路由变量名及views下目录命名

--- src/menu/index.ts
+++ src/menu/index.ts
@@ -4,15 +4,27 @@
 const menu = [
   {
     path: "menu1",
-    component: () => import("@/views/FileRead/FileReadDemo01.vue"),
+    component: () => import("@/views/fileManage/menu01.vue"),
     meta: {
+      groupCode: "fileManage",
       groupTitle: "文件处理",
       title: "FileReader案例",
+      code: "menu1",
     },
   },
   {
-    path: "visualization",
-    component: () => import("@/views/Visualization/Index.vue"),
+    path: "fileRead",
+    component: () => import("@/views/fileManage/fileRead.vue"),
+    meta: {
+      groupCode: "fileManage",
+      groupTitle: "文件处理",
+      title: "各类文件处理",
+      code: "fileRead",
+    },
+  },
+  {
+    path: "firstDraft",
+    component: () => import("@/views/visualization/firstDraft.vue"),
     meta: {
       groupCode: "visualization",
       groupTitle: "可视化",
---
+++ src/views/fileManage/fileRead.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+// todo
+</script>
+
+<template>
+  <h1>fileRead</h1>
+</template>
+
+<style></style>
