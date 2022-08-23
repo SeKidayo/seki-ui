e2bf06a04ea46c12ae19bc27bbf39d1fb5061fc6
seki <seki4649@163.com>
2022-08-23 18:05:28 +0800

    feat:侧边栏导航初步分组

--- src/layout/Aside.vue
+++ src/layout/Aside.vue
@@ -5,35 +5,60 @@ import menu from "@/menu";
 const router = useRouter();
 
 // 提取menu信息
-const menuInfo: MenuInfo[] = menu.map((item) => item.meta);
-console.log(menuInfo);
+const menuList: MenuInfo[] = menu.map((item) => item.meta);
+const menuMap: Record<string, any> = {};
+
+menuList.forEach((item: MenuInfo) => {
+  if (!menuMap[item.groupCode]) {
+    menuMap[item.groupCode] = [];
+  }
+  menuMap[item.groupCode].push(item);
+});
 
 // 路由跳转
 const menuClick = (item: any) => {
-  router.push(`/home/${item.path}`);
+  router.push(`/home/${item.code}`);
 };
 </script>
 
 <template>
   <div class="home-aside-content">
-    <ul>
-      <li v-for="item in menu" :key="item.path">
-        <span class="home-aside-menu-text" @click="menuClick(item)">{{
-          item.meta.title
-        }}</span>
-      </li>
-    </ul>
+    <div
+      v-for="(menuGroup, key) in menuMap"
+      :key="key"
+      class="home-aside-menu-group"
+    >
+      <h3>{{ menuGroup[0]?.groupTitle }}</h3>
+      <ul>
+        <li v-for="item in menuGroup" :key="item.code">
+          <span class="home-aside-menu-text" @click="menuClick(item)">{{
+            item.title
+          }}</span>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <style lang="less" scoped>
 .home-aside-content {
   padding: 10px 20px 0;
-  .home-aside-menu-text {
-    cursor: pointer;
-    &:hover {
-      color: aqua;
-      text-decoration: underline;
+
+  .home-aside-menu-group {
+    margin-bottom: 16px;
+
+    ul > li {
+      font-size: 14px;
+      line-height: 24px;
+      text-indent: 8px;
+    }
+    .home-aside-menu-text {
+      cursor: pointer;
+
+      &:hover {
+        color: aqua;
+        text-decoration: underline;
+      }
     }
   }
 }
