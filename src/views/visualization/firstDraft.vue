e64617c27f7bbd17c60c6110bd27449e42790d09
seki <seki4649@163.com>
2022-08-23 22:04:17 +0800

    fix:页面echarts按需导入

--- src/views/visualization/firstDraft.vue
+++ src/views/visualization/firstDraft.vue
@@ -1,7 +1,78 @@
 <script setup lang="ts">
 import Mock from "mockjs";
-import * as echarts from "echarts";
+// 引入 echarts 核心模块，核心模块提供了 echarts 使用必须要的接口
+import * as echarts from "echarts/core";
+import {
+  // 引入柱状图图表，图表后缀都为 Chart
+  BarChart,
+  // 系列类型的定义后缀都为 SeriesOption
+  BarSeriesOption,
+  LineChart,
+  LineSeriesOption,
+} from "echarts/charts";
+import {
+  // 引入提示框，标题，直角坐标系，数据集，内置数据转换器组件，组件后缀都为 Component
+  TitleComponent,
+  // 组件类型的定义后缀都为 ComponentOption
+  TitleComponentOption,
+  TooltipComponent,
+  TooltipComponentOption,
+  GridComponent,
+  GridComponentOption,
+  // 数据集组件
+  DatasetComponent,
+  DatasetComponentOption,
+  // 内置数据转换器组件 (filter, sort)
+  TransformComponent,
+} from "echarts/components";
+// 标签自动布局，全局过渡动画等特性
+import { LabelLayout, UniversalTransition } from "echarts/features";
+// 引入 Canvas 渲染器，注意引入 CanvasRenderer 或者 SVGRenderer 是必须的一步
+import { CanvasRenderer } from "echarts/renderers";
+
+// 通过 ComposeOption 来组合出一个只有必须组件和图表的 Option 类型
+type ECOption = echarts.ComposeOption<
+  | BarSeriesOption
+  | LineSeriesOption
+  | TitleComponentOption
+  | TooltipComponentOption
+  | GridComponentOption
+  | DatasetComponentOption
+>;
+
+// 注册必须的组件
+echarts.use([
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  BarChart,
+  LineChart,
+  LabelLayout,
+  UniversalTransition,
+  CanvasRenderer,
+]);
 import { onMounted } from "vue";
+
+// 图标配置
+const options: ECOption = {
+  title: {
+    text: "ECharts 入门示例",
+  },
+  tooltip: {},
+  xAxis: {
+    data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+  },
+  yAxis: {},
+  series: [
+    {
+      name: "销量",
+      type: "bar",
+      data: [5, 20, 36, 10, 10, 20],
+    },
+  ],
+};
 
 onMounted(() => {
   // 需要获取到真实DOM,故需要在挂载完毕生命周期内进行 初始化echarts实例 操作
@@ -11,24 +82,9 @@ onMounted(() => {
   const myChart = echarts.init(echartsDom);
 
   // 绘制图标
-  myChart.setOption({
-    title: {
-      text: "ECharts 入门示例",
-    },
-    tooltip: {},
-    xAxis: {
-      data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
-    },
-    yAxis: {},
-    series: [
-      {
-        name: "销量",
-        type: "bar",
-        data: [5, 20, 36, 10, 10, 20],
-      },
-    ],
-  });
+  myChart.setOption(options);
 });
+
 const data = Mock.mock({
   "list|1-10": [
     {
@@ -43,5 +99,3 @@ console.log(data);
 <template>
   <div id="echarts-container" style="width: 600px; height: 400px" />
 </template>
-
-<style></style>
